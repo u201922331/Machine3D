d13f60172b4a67751ca8d635bd1eb8da20a5177f
u201922331 <u201922331@upc.edu.pe>
2022-10-23 18:47:09 -0500

    .

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ import SimpleVoxel as svx
 
 
 def main():
-    cube = handlers.STL.read('models/suzanne.stl')
-    boundaries = cube.boundingBox()
+    model = handlers.STL.read('models/suzanne.stl')
+    boundaries = model.boundingBox()
     xlims = boundaries[:2]
     ylims = boundaries[2:4]
     zlims = boundaries[4:]
-    w = xlims[1]-xlims[0]
-    h = ylims[1]-ylims[0]
-    d = zlims[1]-zlims[0]
+    w = xlims[1] - xlims[0]
+    h = ylims[1] - ylims[0]
+    d = zlims[1] - zlims[0]
 
-    chunk = svx.chunk.Chunk(round(w*10), round(h*10), round(d*10))
+    chunk = svx.chunk.Chunk(round(w * 20), round(h * 20), round(d * 20))
     chunk.plot()
 
 
